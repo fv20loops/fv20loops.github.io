c35270c73452d6aa887142d41e4a06b0fba7abf8
Arthur <>
2020-05-06 22:02:22 +0100

    updates wed 6/5/20

--- cookies.js
+++ cookies.js
@@ -1,9 +1,9 @@
 //a function which checks the flags of aeach page in local storage and tallys them up
 
-function tally() {
+function tallyHome() {
   var x = 0;
   var y = 0;
-  for (i=0; i < 10; i++) {
+  for (i=0; i < 11; i++) {
     x = parseInt(localStorage.getItem("page"+i));
     if (isNaN(x)) {x = 0};
     console.log("page"+i+" "+x);
@@ -11,6 +11,58 @@ function tally() {
     console.log("pageTotal "+y)
   };
   document.getElementById("basket").innerHTML = y;
+
+  for (i=0; i<y; i++) {
+    var newDiv = document.createElement("div");
+    newDiv.setAttribute("class", "logoColumnHome");
+    var newContent = document.createElement("IMG");
+    var selector = Math.random();
+    if (selector<0.5) {
+      newContent.setAttribute("src", "images/loopsLogo/LoopsOFill.png");
+    } else {
+      newContent.setAttribute("src", "images/loopsLogo/LoopsOTarget.png");
+    };
+    // add the new image node to the newly created div
+    newDiv.appendChild(newContent);
+
+    // // add the newly created element and its content into the DOM
+     var currentDiv = document.getElementById("logoBefore");
+     var parentDiv = document.getElementById("logoRow")
+     parentDiv.insertBefore(newDiv, currentDiv);
+  };
+
+}
+
+function tally() {
+  var x = 0;
+  var y = 0;
+  for (i=0; i < 11; i++) {
+    x = parseInt(localStorage.getItem("page"+i));
+    if (isNaN(x)) {x = 0};
+    console.log("page"+i+" "+x);
+    y = y +x;
+    console.log("pageTotal "+y)
+  };
+
+  for (i=0; i<y; i++) {
+    var newDiv = document.createElement("div");
+    newDiv.setAttribute("class", "logoColumn");
+    var newContent = document.createElement("IMG");
+    var selector = Math.random();
+    if (selector<0.5) {
+      newContent.setAttribute("src", "images/loopsLogo/LoopsOFill.png");
+    } else {
+      newContent.setAttribute("src", "images/loopsLogo/LoopsOTarget.png");
+    };
+    // add the new image node to the newly created div
+    newDiv.appendChild(newContent);
+
+    // // add the newly created element and its content into the DOM
+     var currentDiv = document.getElementById("logoBefore");
+     var parentDiv = document.getElementById("logoRow")
+     parentDiv.insertBefore(newDiv, currentDiv);
+  };
+
 }
 
 function pageNumComplete(pageNum) {
@@ -22,6 +74,24 @@ function reset(){
   location.reload();
 }
 
-
-
-  // document.getElementById("basket").innerHTML = x;
+function gameWin() {
+  console.log("win");
+  var x = 0;
+  var y = 0;
+  var arr = [];
+  var chooser = 0;
+  for(i=0; i<11; i++) {
+    x = parseInt(localStorage.getItem("page"+i));
+    if (isNaN(x)) {x=0};
+    if (x==0) {
+      console.log("page"+i+" "+x);
+      arr.push(i);
+      console.log(arr);
+    };
+  }
+  console.log("array length: "+arr.length);
+  chooser = Math.round(Math.random() * (arr.length));
+  console.log("the index of the array...:"+chooser);
+  console.log("page am going to is... "+arr[chooser]);
+  window.location.href='projects/project'+arr[chooser]+'.html';
+}
