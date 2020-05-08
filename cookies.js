7ced78dfb2fbda01f91b05970226ab573578c7a9
Arthur <>
2020-05-08 17:53:15 +0100

    getting there

--- cookies.js
+++ cookies.js
@@ -25,7 +25,7 @@ function tallyHome() {
     x = parseInt(localStorage.getItem("page"+i));
     if (isNaN(x)) {x = 0};
     console.log("page"+i+" "+x);
-    if(x==1) {document.getElementById('project'+i).style.color = "#737373"};
+    if(x==1) {document.getElementById('project'+i).src = "images/loopsLogo/LoopsOTarget.png"};
     y = y +x;
     console.log("pageTotal "+y);
   };
@@ -54,25 +54,102 @@ function tallyHome() {
   function modTally() {
     var x = 0;
     var y = 0;
-    for (i=11; i <15; i++) {
+    for (i=11; i <14; i++) {
       x = parseInt(localStorage.getItem("page"+i));
       if (isNaN(x)) {x = 0};
-      console.log("modpage"+i+" "+x);
+      console.log("modpageOn"+i+" "+x);
     };
     if (localStorage.getItem("page11")==1) {
       console.log("mod1");
-      document.body.style.filter = "invert(1)";
+      document.getElementById("logoRow").style.filter = "invert(1)";
+      document.getElementById("strapline1").style.filter = "invert(1)";
+      document.getElementById("about").style.filter = "invert(1)";
+      document.getElementById("basket").style.filter = "invert(1)";
+      document.getElementById("contact").style.filter = "invert(1)";
+      document.getElementById("game1").style.filter = "invert(1)";
+      document.getElementById("game2").style.filter = "invert(1)";
+      document.getElementById("game3").style.filter = "invert(1)";
+      document.getElementById("game4").style.filter = "invert(1)";
+      document.getElementById("project0").style.filter = "invert(1)";
+      document.getElementById("project1").style.filter = "invert(1)";
+      document.getElementById("project2").style.filter = "invert(1)";
+      document.getElementById("project3").style.filter = "invert(1)";
+      document.getElementById("project4").style.filter = "invert(1)";
+      document.getElementById("project5").style.filter = "invert(1)";
+      document.getElementById("project6").style.filter = "invert(1)";
+      document.getElementById("project7").style.filter = "invert(1)";
+      document.getElementById("project8").style.filter = "invert(1)";
+      document.getElementById("project9").style.filter = "invert(1)";
+      document.getElementById("project10").style.filter = "invert(1)";
+      document.getElementById("mod1").style.filter = "invert(1)";
+      document.getElementById("mod2").style.filter = "invert(1)";
+      document.getElementById("mod3").style.filter = "invert(1)";
     };
     if (localStorage.getItem("page12")==1) {
       console.log("mod2");
-      document.body.style.filter = "blur(3px)";
+      document.getElementById("logoRow").style.filter = "blur(3px)";
+      document.getElementById("strapline1").style.filter = "blur(3px)";
+      document.getElementById("about").style.filter = "blur(3px)";
+      document.getElementById("basket").style.filter = "blur(3px)";
+      document.getElementById("contact").style.filter = "blur(3px)";
+      document.getElementById("game1").style.filter = "blur(3px)";
+      document.getElementById("game2").style.filter = "blur(3px)";
+      document.getElementById("game3").style.filter = "blur(3px)";
+      document.getElementById("game4").style.filter = "blur(3px)";
+      document.getElementById("project0").style.filter = "blur(3px)";
+      document.getElementById("project1").style.filter = "blur(3px)";
+      document.getElementById("project2").style.filter = "blur(3px)";
+      document.getElementById("project3").style.filter = "blur(3px)";
+      document.getElementById("project4").style.filter = "blur(3px)";
+      document.getElementById("project5").style.filter = "blur(3px)";
+      document.getElementById("project6").style.filter = "blur(3px)";
+      document.getElementById("project7").style.filter = "blur(3px)";
+      document.getElementById("project8").style.filter = "blur(3px)";
+      document.getElementById("project9").style.filter = "blur(3px)";
+      document.getElementById("project10").style.filter = "blur(3px)";
+      document.getElementById("mod1").style.filter = "blur(3px)";
+      document.getElementById("mod2").style.filter = "blur(3px)";
+      document.getElementById("mod3").style.filter = "blur(3px)";
     };
     if (localStorage.getItem("page13")==1) {
       console.log("mod3");
-      document.body.style.filter = "brightness(2) saturate(30%) sepia(10%)";
+      document.getElementById("logoRow").style.filter = "brightness(2) saturate(30%) sepia(10%)";
+      document.getElementById("strapline1").style.filter = "brightness(2) saturate(30%) sepia(10%)";
+      document.getElementById("about").style.filter = "brightness(2) saturate(30%) sepia(10%)";
+      document.getElementById("basket").style.filter = "brightness(2) saturate(30%) sepia(10%)";
+      document.getElementById("contact").style.filter = "brightness(2) saturate(30%) sepia(10%)";
+      document.getElementById("game1").style.filter = "brightness(2) saturate(30%) sepia(10%)";
+      document.getElementById("game2").style.filter = "brightness(2) saturate(30%) sepia(10%)";
+      document.getElementById("game3").style.filter = "brightness(2) saturate(30%) sepia(10%)";
+      document.getElementById("game4").style.filter = "brightness(2) saturate(30%) sepia(10%)";
+      document.getElementById("project0").style.filter = "brightness(2) saturate(30%) sepia(10%)";
+      document.getElementById("project1").style.filter = "brightness(2) saturate(30%) sepia(10%)";
+      document.getElementById("project2").style.filter = "brightness(2) saturate(30%) sepia(10%)";
+      document.getElementById("project3").style.filter = "brightness(2) saturate(30%) sepia(10%)";
+      document.getElementById("project4").style.filter = "brightness(2) saturate(30%) sepia(10%)";
+      document.getElementById("project5").style.filter = "brightness(2) saturate(30%) sepia(10%)";
+      document.getElementById("project6").style.filter = "brightness(2) saturate(30%) sepia(10%)";
+      document.getElementById("project7").style.filter = "brightness(2) saturate(30%) sepia(10%)";
+      document.getElementById("project8").style.filter = "brightness(2) saturate(30%) sepia(10%)";
+      document.getElementById("project9").style.filter = "brightness(2) saturate(30%) sepia(10%)";
+      document.getElementById("project10").style.filter = "brightness(2) saturate(30%) sepia(10%)";
+      document.getElementById("mod1").style.filter = "brightness(2) saturate(30%) sepia(10%)";
+      document.getElementById("mod2").style.filter = "brightness(2) saturate(30%) sepia(10%)";
+      document.getElementById("mod3").style.filter = "brightness(2) saturate(30%) sepia(10%)";
 
   };
-    if (localStorage.getItem("page14")==1) {console.log("mod4")};
+    if (localStorage.getItem("page14")==1) {
+      console.log("mod1Flag");
+      document.getElementById("mod1").src = "images/loopsLogo/LoopsO.png"
+    };
+    if (localStorage.getItem("page15")==1) {
+      console.log("mod2Flag");
+      document.getElementById("mod2").src = "images/loopsLogo/LoopsO.png"
+    };
+    if (localStorage.getItem("page15")==1) {
+      console.log("mod3Flag");
+      document.getElementById("mod3").src = "images/loopsLogo/LoopsO.png"
+    };
 
   };
 
@@ -123,7 +200,7 @@ function gameWin() {
   var y = 0;
   var arr = [];
   var chooser = 0;
-  for (i=11; i<15; i++) {
+  for (i=11; i<14; i++) {
     x = parseInt(localStorage.getItem("page"+i));
     if (isNaN(x)) {x=0};
     if (x==1) {
@@ -154,13 +231,16 @@ function gameWin() {
       console.log(arr);
     };
   }
+  if (arr.length > 0) {
   console.log("array length: "+arr.length);
   chooser = Math.round(Math.random() * (arr.length-1));
   console.log("the index of the array...:"+chooser);
   console.log("page am going to is... "+arr[chooser]);
   setTimeout(function(){window.location.href='projects/project'+arr[chooser]+'.html';}, 1000);
-
-  }
+} else {
+  setTimeout(function(){window.location.href='winner.html';}, 1000);
+}
+}
 }
 
 function gameLose() {
